8a34b6a1942c56cb98e5fbc53a11f79725383886
Tobias Bengfort <tobias.bengfort@posteo.de>
2021-06-29 17:45:36 +0200

    refine admin

--- mfa/admin.py
+++ mfa/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import MFAKey
 
-admin.site.register(MFAKey)
+
+@admin.register(MFAKey)
+class MFAKeyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'method']
