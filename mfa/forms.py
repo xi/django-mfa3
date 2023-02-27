d5f28408047eb6bdffc8b567e65890a56eaf8303
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-02-27 16:37:10 +0100

    autofocus in auth form

--- mfa/forms.py
+++ mfa/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 
-class MFAAuthForm(forms.Form):
+class MFABaseForm(forms.Form):
     code = forms.CharField(label=_('Authentication code'))
 
     def __init__(self, validate_code=None, **kwargs):
@@ -20,5 +20,11 @@ class MFAAuthForm(forms.Form):
         return cleaned_data
 
 
-class MFACreateForm(MFAAuthForm):
+class MFAAuthForm(MFABaseForm):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.fields['code'].widget.attrs.update({'autofocus': True})
+
+
+class MFACreateForm(MFABaseForm):
     name = forms.CharField(label=_('Name'), max_length=32)
