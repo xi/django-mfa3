5ff3a84ba0a9e65451aac93d54aa156ccdb8d1f3
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-07-31 19:30:27 +0200

    rm manage.py

--- tests/manage.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-from django.core.management import execute_from_command_line
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
-execute_from_command_line(sys.argv)
